e6a58c5fad476c86a10b139e49eba0e127a11130
Alma Guadalupe <Alma Guadalupe@LAPTOP-Q9EEB5AM>
2022-05-17 19:00:56 -0500

    MiCalculadora

---
+++ src/main/java/com/Alma/CalculadoraAlmaApplication.java
@@ -0,0 +1,13 @@
+package com.Alma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraAlmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraAlmaApplication.class, args);
+	}
+
+}
